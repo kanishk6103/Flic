4b20c3d452245624eb336393e9f8d064980e361d
Mohit Aasirwal <mohitaasirwal@gmail.com>
2022-09-30 16:00:51 +0530

    gradient in text

--- src/components/HomeHero.js
+++ src/components/HomeHero.js
@@ -6,7 +6,10 @@ const HomeHero = () => {
       <div className="grid h-full overflow-x-hidden bg-slate-900">
         <div className="mx-auto sm:mx-16 flex flex-col pt-20">
           <h1 className="text-white text-5xl font-semibold w-80 leading-[5rem]">
-            Hey! Welcome to Our Club <span className="text-gradient-to-r from-indigo-500 to-blue-500 font-bold">FLiC.</span>
+            Hey! Welcome to Our Club{" "}
+            <span className="bg-gradient-to-r from-indigo-800 to-green-400 text-transparent bg-clip-text font-bold">
+              FLiC.
+            </span>
           </h1>
           <p className="text-slate-400 my-4">
             The official financial literacy club of IET-DAVV.
@@ -29,10 +32,10 @@ const HomeHero = () => {
         </div>
         <div className="grid grid-cols-1 sm:flex w-[100vw] space-y-8 mt-10 sm:space-y-0 sm:pt-10">
           <div className="mx-auto sm:mx-16">
-          <div className="flex">
-            <img src="" alt="#" />
-            <h2 className="text-white font-semibold text-lg">Features</h2>
-          </div>
+            <div className="flex">
+              <img src="" alt="#" />
+              <h2 className="text-white font-semibold text-lg">Features</h2>
+            </div>
             <p className="w-72 text-white ">
               Lorem ipsum dolor sit amet coctetur adipishhgging elit. Ratione
               quae odio perferendis fugiat possimus dicta. Corporis recusandae
@@ -40,10 +43,10 @@ const HomeHero = () => {
             </p>
           </div>
           <div className="mx-auto sm:mx-4  pb-10 ">
-          <div className="flex">
-            <img src="" alt="#" />
-            <h2 className="text-white font-semibold text-lg">Features</h2>
-          </div>
+            <div className="flex">
+              <img src="" alt="#" />
+              <h2 className="text-white font-semibold text-lg">Features</h2>
+            </div>
             <p className="w-72 text-white ">
               Lorem ipsum dolor sit amet coctetur adipishhgging elit. Ratione
               quae odio perferendis fugiat possimus dicta. Corporis recusandae
