0554c478978c7507382630c13cfda0f287be9c8d
MustafaAzad <mustafaazad533@gmail.com>
2022-10-01 09:24:14 +0530

    Horizontal Scroll

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Contact from './components/Contact'
 import HomeHero from './components/HomeHero'
 import Navbar from './components/Navbar'
 import Teams from './components/Teams'
 import Timeline from './components/Timeline'
+import locomotiveScroll from 'locomotive-scroll'
 import './index.css'
 
 const App = () => {
+  const ref = useRef(null);
+  useEffect(() => {
+    if(ref){
+      new locomotiveScroll({
+        el : ref.current,
+        smooth :true,
+        direction : 'horizontal',
+        multiplier : 0.5
+      })
+    }
+  }, [])
   return (
     <div>
-        <Navbar/>
+        {/* <Navbar/> */}
+        <div className='flex w-[400vw] h-[100vh] overflow-y-hidden' ref={ref} data-scroll-container>
         <HomeHero/>
         <Timeline/>
         <Teams/>
         <Contact/>
+        </div>
     </div>
   )
 }
