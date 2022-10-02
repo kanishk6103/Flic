78008f42cc3c249d5e3dee0e2a517949b3ce6172
MustafaAzad <mustafaazad533@gmail.com>
2022-10-02 17:13:55 +0530

    Animation In Landing and Timeline Page

--- src/components/HomeHero.js
+++ src/components/HomeHero.js
@@ -1,39 +1,39 @@
 import React from "react";
-
+import { motion } from "framer-motion";
 const HomeHero = () => {
   return (
     <>
       <div className="grid sm:h-full overflow-x-hidden bg-slate-900">
         <div className="mx-auto sm:mx-16 flex flex-col pt-8">
-          <h1 className="text-white text-4xl font-semibold w-80 leading-[5rem]">
+          <motion.h1 initial={{opacity:0,y:100}} animate={{opacity:1,y:0}} transition={{delay:0.5,duration:1.5,bounce:2}} className="text-white text-4xl font-semibold w-80 leading-[5rem]">
             Hey! Welcome to Our Club{" "}
             <span className="bg-gradient-to-r from-[#2F2A7B] text-6xl to-[#39B54A] text-transparent bg-clip-text font-bold">
               FLiC.
             </span>
-          </h1>
-          <p className="text-[#949494] my-4">
+          </motion.h1>
+          <motion.p initial={{opacity:0,y:100}} animate={{opacity:1,y:0}} transition={{delay:0.9,duration:1.5,bounce:2}} className="text-[#949494] my-4">
             The official financial literacy club of IET-DAVV.
-          </p>
+          </motion.p>
 
           <div className="flex space-x-10">
-            <button
+            <motion.button initial={{opacity:0,x:-100}} animate={{opacity:1,x:0}} transition={{delay:1.2,duration:1.5,bounce:2}}
               type="button"
               className="px-8 py-3 active:rounded font-semibold rounded-full hover:bg-green-700 bg-green-600 text-gray-800"
             >
               Sample
-            </button>
-            <button
+            </motion.button>
+            <motion.button initial={{opacity:0,x:100}} animate={{opacity:1,x:0}} transition={{delay:1.2,duration:1.5,bounce:2}}
               type="button"
               className="px-8 active:rounded  bg-violet-800 hover:bg-violet-900 py-3 font-semibold rounded-full"
             >
               Sample
-            </button>
+            </motion.button>
           </div>
         </div>
         <div className="grid grid-cols-1 sm:flex w-[100vw] space-y-8 mt-8 sm:space-y-0 sm:pt-10">
           <div className="mx-auto sm:mx-16">
             <div className="flex">
-              <svg
+              <motion.svg initial={{opacity:0,scale:0.5}} animate={{opacity:1, scale:1}} transition={{delay:1.5,duration:0.7}}
                 width="40"
                 height="40"
                 className="mx-4"
@@ -78,18 +78,18 @@ const HomeHero = () => {
                   stroke="#969696"
                 />
                 <line x1="13" y1="29.5" x2="54" y2="29.5" stroke="#969696" />
-              </svg>
-              <h2 className="text-white font-semibold text-lg">Features</h2>
+              </motion.svg>
+              <motion.h2 initial={{opacity:0,y:100}} animate={{opacity:1,y:0}} transition={{delay:1,duration:1.1,bounce:2}} className="text-white font-semibold text-lg">Features</motion.h2>
             </div>
-            <p className="w-60 ml-[4.2rem] text-sm text-[#828282] ">
+            <motion.p initial={{opacity:0,y:100}} animate={{opacity:1,y:0}} transition={{delay:1.2,duration:1.1,bounce:2}} className="w-60 ml-[4.2rem] text-sm text-[#828282] ">
               Lorem ipsum dolor sit amet coctetur adipishhgging elit. Ratione
               quae odio perferendis fugiat possimus. Corporis aliquam ipsum
               debitis possimus similique molestias
-            </p>
+            </motion.p>
           </div>
           <div className="mx-auto sm:mx-4  pb-10 ">
             <div className="flex">
-              <svg
+              <motion.svg initial={{opacity:0,scale:0.5}} animate={{opacity:1, scale:1}} transition={{delay:1.5,duration:0.7}}
                 width="40"
                 height="40"
                 className="mx-4"
@@ -134,14 +134,14 @@ const HomeHero = () => {
                   stroke="#969696"
                 />
                 <line x1="13" y1="29.5" x2="54" y2="29.5" stroke="#969696" />
-              </svg>
-              <h2 className="text-white font-semibold text-lg">Features</h2>
+              </motion.svg>
+              <motion.h2 initial={{opacity:0,y:100}} animate={{opacity:1,y:0}} transition={{delay:1,duration:1.1,bounce:2}} className="text-white font-semibold text-lg">Features</motion.h2>
             </div>
-            <p className="w-60 ml-[4.2rem] text-sm text-[#828282] ">
+            <motion.p initial={{opacity:0,y:100}} animate={{opacity:1,y:0}} transition={{delay:1.2,duration:1.1,bounce:2}} className="w-60 ml-[4.2rem] text-sm text-[#828282] ">
               Lorem ipsum dolor sit amet coctetur adipishhgging elit. Ratione
               quae odio perferendis fugiat possimus. Corporis aliquam ipsum
               debitis possimus similique molestias
-            </p>
+            </motion.p>
           </div>
         </div>
       </div>
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,23 +1,23 @@
+import { motion } from "framer-motion";
 import React from "react";
-
 const Timeline = () => {
 	return (
 		<div id="About">
 			<div class="overflow-hidden">
 				<div class="flex flex-col bg-slate-900 md:grid grid-cols-9 mx-auto p-2   text-blue-50">
 					<div class="flex flex-row-reverse md:contents">
-						<div class="col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md">
+						<motion.div initial={{opacity:0,x:-100}} whileInView={{opacity:1,x:0}} transition={{duration:1.2,bounce:1}} class="col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md">
 							<h3 class=" font-bold justify-center mb-3 bg-gradient-to-r from-indigo-800 to-green-400 text-transparent bg-clip-text text-4xl">What is FLiC</h3>
-							<p class="leading-tight text-justify">
+							<p class="leading-tight text-[#D6D6D6] text-justify">
 								Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
 								quaerat?Lorem ipsum dolor sit amet consectetur adipisicing elit.
 								Tenetur, hic minima. Corrupti voluptas vitae tenetur commodi
 								error cumque consequuntur neque possimus. Molestias.
 							</p>
-						</div>
+						</motion.div>
 						<div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
 							<div class="h-full w-6 flex items-center justify-center">
-								<div class="h-full w-1 bg-slate-400 pointer-events-none"></div>
+								<div class="h-full w-1 bg-white/60 pointer-events-none"></div>
 							</div>
 							<div class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-white shadow"></div>
 						</div>
@@ -26,31 +26,35 @@ const Timeline = () => {
 					<div class="flex md:contents">
 						<div class="col-start-5 col-end-6 mr-10 md:mx-auto relative">
 							<div class="h-full w-6 flex items-center justify-center">
-								<div class="h-full w-1 bg-blue-800 pointer-events-none"></div>
+								<div class="h-full w-1 bg-white/60 pointer-events-none"></div>
 							</div>
-							<div class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-500 shadow"></div>
+							<div class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-white shadow"></div>
 						</div>
-						<div class="bg-blue-500 col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md">
-							<h3 class="font-semibold text-lg mb-1">Lorem ipsum</h3>
-							<p class="leading-tight text-justify">
-								Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae,
-								facilis.
+						<motion.div initial={{opacity:0,x:100}} whileInView={{opacity:1,x:0}} transition={{duration:1.2,bounce:1}} class=" col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md">
+							<h3 class=" justify-center mb-3 bg-gradient-to-r from-indigo-800 to-green-400 text-transparent bg-clip-text font-bold text-4xl">Why is FLiC</h3>
+							<p class="leading-tight text-[#D6D6D6] text-justify">
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+								quaerat?Lorem ipsum dolor sit amet consectetur adipisicing elit.
+								Tenetur, hic minima. Corrupti voluptas vitae tenetur commodi
+								error cumque consequuntur neque possimus. Molestias.
 							</p>
-						</div>
+						</motion.div>
 					</div>
 					<div class="flex flex-row-reverse md:contents">
-						<div class="bg-blue-500 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md">
-							<h3 class="font-semibold text-lg mb-1">Lorem ipsum</h3>
-							<p class="leading-tight text-justify">
+						<motion.div initial={{opacity:0,x:-100}} whileInView={{opacity:1,x:0}} transition={{duration:1.2,bounce:1}} class="col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md">
+							<h3 class="justify-center mb-3 bg-gradient-to-r from-indigo-800 to-green-400 text-transparent bg-clip-text font-bold text-4xl">Our Mission</h3>
+							<p class="leading-tight text-[#D6D6D6] text-justify">
 								Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
-								quaerat?
+								quaerat?Lorem ipsum dolor sit amet consectetur adipisicing elit.
+								Tenetur, hic minima. Corrupti voluptas vitae tenetur commodi
+								error cumque consequuntur neque possimus. Molestias.
 							</p>
-						</div>
+						</motion.div>
 						<div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
 							<div class="h-full w-6 flex items-center justify-center">
-								<div class="h-full w-1 bg-blue-800 pointer-events-none"></div>
+								<div class="h-full w-1 bg-white/60 pointer-events-none"></div>
 							</div>
-							<div class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-500 shadow"></div>
+							<div class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-white shadow"></div>
 						</div>
 					</div>
 				</div>
