2fabea75b40a57e1f5a5e2c4df679e8ff78e33d7
kanishk6103 <mamta11tiwari06@gmail.com>
2022-10-05 23:49:00 +0530

    Added frontend improvements

--- src/components/HomeHero.js
+++ src/components/HomeHero.js
@@ -3,34 +3,34 @@ import { motion } from "framer-motion";
 const HomeHero = () => {
   return (
     <>
-      <div className="grid sm:h-full overflow-x-hidden bg-slate-900">
-        <div className="mx-auto sm:mx-16 flex flex-col pt-8">
-          <motion.h1 initial={{opacity:0,y:100}} animate={{opacity:1,y:0}} transition={{delay:0.5,duration:1.5,bounce:2}} className="text-white text-4xl font-semibold w-80 leading-[5rem]">
-            Hey! Welcome to Our Club{" "}
-            <span className="bg-gradient-to-r from-[#2F2A7B] text-6xl to-[#39B54A] text-transparent bg-clip-text font-bold">
+      <div className="grid sm:h-screen overflow-x-hidden bg-[#202020]">
+        <div className="mx-auto sm:mx-32 flex flex-col pt-48">
+          <motion.h1 initial={{opacity:0,y:100}} animate={{opacity:1,y:0}} transition={{delay:0.5,duration:1.5,bounce:2}} className="font-['Epilogue'] antialiased text-white text-4xl font-[500] w-96 leading-[5rem] tracking-wide">
+            Hey! Welcome to our club{" "}
+            <span className="bg-gradient-to-r from-[#2F2A7B] text-7xl to-[#39B54A] text-transparent bg-clip-text pl-2 font-[900] tracking-wide">
               FLiC.
             </span>
           </motion.h1>
-          <motion.p initial={{opacity:0,y:100}} animate={{opacity:1,y:0}} transition={{delay:0.9,duration:1.5,bounce:2}} className="text-[#949494] my-4">
+          <motion.p initial={{opacity:0,y:100}} animate={{opacity:1,y:0}} transition={{delay:0.9,duration:1.5,bounce:2}} className="font-['Epilogue'] font-[200] antialiased text-[#949494] my-4 pb-16">
             The official financial literacy club of IET-DAVV.
           </motion.p>
 
           <div className="flex space-x-10">
             <motion.button initial={{opacity:0,x:-100}} animate={{opacity:1,x:0}} transition={{delay:1.2,duration:1.5,bounce:2}}
               type="button"
-              className="px-8 py-3 active:rounded font-semibold rounded-full hover:bg-green-700 bg-green-600 text-gray-800"
+              className="px-8 py-3 active:rounded font-['Epilogue'] font-[600] antialiased rounded-full bg-[#338D3F] hover:bg-green-600 text-white"
             >
               Sample
             </motion.button>
             <motion.button initial={{opacity:0,x:100}} animate={{opacity:1,x:0}} transition={{delay:1.2,duration:1.5,bounce:2}}
               type="button"
-              className="px-8 active:rounded  bg-violet-800 hover:bg-violet-900 py-3 font-semibold rounded-full"
+              className="px-8 py-3 active:rounded bg-[#262262] font-['Epilogue'] font-[600] hover:bg-[#453FA6] py-3 font-semibold rounded-full text-white"
             >
               Sample
             </motion.button>
           </div>
         </div>
-        <div className="grid grid-cols-1 sm:flex w-[100vw] space-y-8 mt-8 sm:space-y-0 sm:pt-10">
+        <div className="pl-12 grid grid-cols-1 sm:flex w-[100vw] space-y-8 mt-8 sm:space-y-0 sm:pt-20">
           <div className="mx-auto sm:mx-16">
             <div className="flex">
               <motion.svg initial={{opacity:0,scale:0.5}} animate={{opacity:1, scale:1}} transition={{delay:1.5,duration:0.7}}
@@ -81,13 +81,13 @@ const HomeHero = () => {
               </motion.svg>
               <motion.h2 initial={{opacity:0,y:100}} animate={{opacity:1,y:0}} transition={{delay:1,duration:1.1,bounce:2}} className="text-white font-semibold text-lg">Features</motion.h2>
             </div>
-            <motion.p initial={{opacity:0,y:100}} animate={{opacity:1,y:0}} transition={{delay:1.2,duration:1.1,bounce:2}} className="w-60 ml-[4.2rem] text-sm text-[#828282] ">
+            <motion.p initial={{opacity:0,y:100}} animate={{opacity:1,y:0}} transition={{delay:1.2,duration:1.1,bounce:2}} className="w-60 ml-[4.2rem] text-sm text-[#828282]">
               Lorem ipsum dolor sit amet coctetur adipishhgging elit. Ratione
               quae odio perferendis fugiat possimus. Corporis aliquam ipsum
               debitis possimus similique molestias
             </motion.p>
           </div>
-          <div className="mx-auto sm:mx-4  pb-10 ">
+          <div className="mx-auto sm:mx-4  pb-10">
             <div className="flex">
               <motion.svg initial={{opacity:0,scale:0.5}} animate={{opacity:1, scale:1}} transition={{delay:1.5,duration:0.7}}
                 width="40"
@@ -137,7 +137,7 @@ const HomeHero = () => {
               </motion.svg>
               <motion.h2 initial={{opacity:0,y:100}} animate={{opacity:1,y:0}} transition={{delay:1,duration:1.1,bounce:2}} className="text-white font-semibold text-lg">Features</motion.h2>
             </div>
-            <motion.p initial={{opacity:0,y:100}} animate={{opacity:1,y:0}} transition={{delay:1.2,duration:1.1,bounce:2}} className="w-60 ml-[4.2rem] text-sm text-[#828282] ">
+            <motion.p initial={{opacity:0,y:100}} animate={{opacity:1,y:0}} transition={{delay:1.2,duration:1.1,bounce:2}} className="w-60 ml-[4.2rem] text-sm text-[#828282]">
               Lorem ipsum dolor sit amet coctetur adipishhgging elit. Ratione
               quae odio perferendis fugiat possimus. Corporis aliquam ipsum
               debitis possimus similique molestias
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,8 +6,9 @@ import { motion } from "framer-motion";
 const Navbar = () => {
 
   return (
-    <nav className="w-full fixed bg-slate-900 shadow">
-      <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+    <nav className="flex w-screen fixed bg-[#202020] shadow">
+      <div className="justify-start px-4 lg:max-w-7xl md:items-center md:flex md:px-8">
+      <div className="flex items-center justify-between pl-24 py-3 md:py-8 md:block">
 
         {/* LOGO animation -> svg*/}
         <svg id="svg" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="157.71 288.18 764.59 503.63" shapeRendering="geometricPrecision" textRendering="geometricPrecision">
@@ -16,7 +17,7 @@ const Navbar = () => {
               <stop offset={0} style={{stopColor: '#2F2A7B'}} />
               <stop offset={1} style={{stopColor: '#39B54A'}} />
             </linearGradient>
-            
+
             <mask id="svg-mask" maskUnits="userSpaceOnUse" x={0} y={0} width={1080} height={1080}>
               <path id="pathsvg" d="M169,786.34Q363.359996,175.978707,899.999998,336.45q-179.120002,587.259106-529.72,350.82q69.719998-251.25,299.72-209.26" fill="none" strokeWidth={35} strokeLinecap="round" strokeLinejoin="miter" />
             </mask>
@@ -27,17 +28,22 @@ const Navbar = () => {
             <rect id="svg-gradient" x={0} y={0} width={1080} height={1080} fill="url(#gradient)" />
           </g>
         </svg>
-
+        </div>
         <div>
-            <ul className="items-center justify-center space-y-8 md:mx-8 md:my-8 md:flex md:space-x-6 md:space-y-0">
-              <motion.li initial={{opacity:0,x:-100}} animate={{opacity:1,x:0}} transition={{delay:0.8,duration:1,bounce:1}} className="text-white/80 hover:font-semibold hover:text-white">
+            <ul className="items-center justify-center space-y-8 md:flex md:space-x-24 md:space-y-0 pt-auto text-lg font-['Epilogue'] font-medium antialiased">
+
+            <motion.li initial={{opacity:0,x:-100}} animate={{opacity:1,x:0}} transition={{delay:0.8,duration:1,bounce:1}} className="text-[#D6D6D6] pl-16 font-[100] text-4xl">
+              <a href="#line">|</a>
+            </motion.li>
+
+              <motion.li initial={{opacity:0,x:-100}} animate={{opacity:1,x:0}} transition={{delay:0.8,duration:1,bounce:1}} className="text-[#D6D6D6] hover:text-[#ffffff] ml-16">
                 <a href="#About">About Us</a>
               </motion.li>
-              <motion.li initial={{opacity:0,x:-100}} animate={{opacity:1,x:0}} transition={{delay:1.3,duration:1,bounce:1}} className="text-white/80 hover:font-semibold hover:text-white">
+              <motion.li initial={{opacity:0,x:-100}} animate={{opacity:1,x:0}} transition={{delay:1.3,duration:1,bounce:1}} className="text-[#D6D6D6] hover:text-[#ffffff]">
                 <a href="#Teams">Our Team</a>
               </motion.li>
-              <motion.li initial={{opacity:0,x:-100}} animate={{opacity:1,x:0}} transition={{delay:1.8,duration:1,bounce:1}} className="text-white/80 hover:font-semibold hover:text-white">
-                <a href="#contact">Contact Us</a>
+              <motion.li initial={{opacity:0,x:-100}} animate={{opacity:1,x:0}} transition={{delay:1.8,duration:1,bounce:1}} className="text-[#D6D6D6] hover:text-[#ffffff]">
+                <a href="#contact">Contact</a>
               </motion.li>
             </ul>
           </div>
