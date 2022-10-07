541f825d2a65460dc038d0da4a3f66f517519955
MustafaAzad <mustafaazad533@gmail.com>
2022-10-07 12:42:07 +0530

    Fixed Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,16 +8,16 @@ const Navbar = () => {
   return (
     <nav className="flex w-screen fixed bg-[#202020] shadow">
       <div className="justify-start px-4 lg:max-w-7xl md:items-center md:flex md:px-8">
-      <div className="flex items-center justify-between pl-24 py-3 md:py-8 md:block">
+      <div className="flex items-center justify-between pl-24 py-3 md:py-2 md:block">
 
         {/* LOGO animation -> svg*/}
-        <svg id="svg" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="157.71 288.18 764.59 503.63" shapeRendering="geometricPrecision" textRendering="geometricPrecision">
+        <div className="mt-2">
+        <svg id="svg" width={30} height={30} xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="157.71 288.18 764.59 503.63" shapeRendering="geometricPrecision" textRendering="geometricPrecision">
           <defs>
             <linearGradient id="gradient" gradientUnits="userSpaceOnUse" fy="90%" gradientTransform="rotate(-11)">
               <stop offset={0} style={{stopColor: '#2F2A7B'}} />
               <stop offset={1} style={{stopColor: '#39B54A'}} />
             </linearGradient>
-
             <mask id="svg-mask" maskUnits="userSpaceOnUse" x={0} y={0} width={1080} height={1080}>
               <path id="pathsvg" d="M169,786.34Q363.359996,175.978707,899.999998,336.45q-179.120002,587.259106-529.72,350.82q69.719998-251.25,299.72-209.26" fill="none" strokeWidth={35} strokeLinecap="round" strokeLinejoin="miter" />
             </mask>
@@ -28,6 +28,7 @@ const Navbar = () => {
             <rect id="svg-gradient" x={0} y={0} width={1080} height={1080} fill="url(#gradient)" />
           </g>
         </svg>
+        </div>
         </div>
         <div>
             <ul className="items-center justify-center space-y-8 md:flex md:space-x-24 md:space-y-0 pt-auto text-lg font-['Epilogue'] font-medium antialiased">
